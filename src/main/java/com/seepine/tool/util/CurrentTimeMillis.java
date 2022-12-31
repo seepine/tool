f7fffcdce77f77468a81d243c61bc519495689f0
seepine <seepine@outlook.com>
2022-12-31 19:39:01 +0800

    feat(util): add currentTimeMillis

---
+++ src/main/java/com/seepine/tool/util/CurrentTimeMillis.java
@@ -0,0 +1,42 @@
+package com.seepine.tool.util;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 获取当前时间，用于高并发下替代System.currentTimeMillis()
+ *
+ * @author seepine
+ * @since 0.0.9
+ */
+public class CurrentTimeMillis {
+  private volatile long now;
+
+  private CurrentTimeMillis() {
+    this.now = System.currentTimeMillis();
+    scheduleTick();
+  }
+
+  private void scheduleTick() {
+    new ScheduledThreadPoolExecutor(
+            1,
+            runnable -> {
+              Thread thread = new Thread(runnable, "tool-current-time-millis");
+              thread.setDaemon(true);
+              return thread;
+            })
+        .scheduleAtFixedRate(() -> now = System.currentTimeMillis(), 1, 1, TimeUnit.MILLISECONDS);
+  }
+
+  public static long now() {
+    return getInstance().now;
+  }
+
+  private static CurrentTimeMillis getInstance() {
+    return SingletonHolder.INSTANCE;
+  }
+
+  private static class SingletonHolder {
+    private static final CurrentTimeMillis INSTANCE = new CurrentTimeMillis();
+  }
+}
