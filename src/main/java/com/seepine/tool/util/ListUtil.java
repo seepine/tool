9c42486c6874c1c3ebaa211efde94412b5d1d44d
seepine <seepine@outlook.com>
2023-07-13 11:44:25 +0800

    feat(util/list): add sort func

--- src/main/java/com/seepine/tool/util/ListUtil.java
+++ src/main/java/com/seepine/tool/util/ListUtil.java
@@ -1,13 +1,14 @@
 package com.seepine.tool.util;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author seepine
@@ -77,6 +78,24 @@ public class ListUtil {
   public static <T> List<T> fromArray(@Nonnull T... arr) {
     List<T> list = new ArrayList<>();
     Collections.addAll(list, arr);
+    return list;
+  }
+
+  /**
+   * 针对List排序，排序会修改原List
+   *
+   * @param <T> 元素类型
+   * @param list 被排序的List
+   * @param c {@link Comparator}
+   * @return 原list
+   * @see Collections#sort(List, Comparator)
+   */
+  @Nonnull
+  public static <T> List<T> sort(@Nullable List<T> list, @Nonnull Comparator<? super T> c) {
+    if (Objects.isEmpty(list)) {
+      return new ArrayList<>();
+    }
+    list.sort(c);
     return list;
   }
 }
